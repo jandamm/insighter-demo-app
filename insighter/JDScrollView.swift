9a05b3d4302119507d301ce2d9b026d04c07492f
Jan Dammshäuser <jandammshaeuser@me.com>
2016-08-10 10:40:02 +0200

    Added
    - JDScrollView

---
+++ insighter/JDScrollView.swift
@@ -0,0 +1,57 @@
+//
+//  JDScrollView.swift
+//  insighter
+//
+//  Created by Jan Dammshäuser on 10.08.16.
+//  Copyright © 2016 Jan Dammshäuser. All rights reserved.
+//
+
+import UIKit
+
+class JDScrollView: UIScrollView {
+    
+    @IBOutlet weak var viewController: UIViewController!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupNotificationListenersForKeyboardEvents()
+        setupScrollView()
+    }
+    
+    
+    // MARK: - Textfield - Scrollview Methods
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        var userInfo = notification.userInfo!
+        var keyboardFrame: CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        keyboardFrame = viewController.view.convertRect(keyboardFrame, fromView: nil)
+        
+        var contentInset: UIEdgeInsets = self.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        self.contentInset = contentInset
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        
+        let contentInset:UIEdgeInsets = UIEdgeInsetsZero
+        self.contentInset = contentInset
+    }
+    
+    
+    // MARK: - Private Methods
+    
+    private func setupScrollView() {
+        showsVerticalScrollIndicator = false
+        showsHorizontalScrollIndicator = false
+        
+        keyboardDismissMode = .Interactive
+    }
+    
+    private func setupNotificationListenersForKeyboardEvents() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow), name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide), name:UIKeyboardWillHideNotification, object: nil)
+    }
+
+}
--- insighter/OnboardingLoginVC.swift
+++ insighter/OnboardingLoginVC.swift
@@ -9,14 +9,9 @@
 import UIKit
 
 class OnboardingLoginVC: UIViewController {
-
-    @IBOutlet weak var scrollView: UIScrollView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupNotificationListenersForKeyboardEvents()
     }
     
 
@@ -34,39 +29,6 @@ class OnboardingLoginVC: UIViewController {
     
     @IBAction func loginPressed(sender: AnyObject) {
         print("button pressed")
-    }
-    
-    
-    // MARK: - Textfield - Scrollview Methods
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    func keyboardWillShow(notification: NSNotification) {
-        
-        var userInfo = notification.userInfo!
-        var keyboardFrame: CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
-        keyboardFrame = view.convertRect(keyboardFrame, fromView: nil)
-        
-        var contentInset: UIEdgeInsets = scrollView.contentInset
-        contentInset.bottom = keyboardFrame.size.height
-        scrollView.contentInset = contentInset
-    }
-    
-    func keyboardWillHide(notification: NSNotification) {
-        
-        let contentInset:UIEdgeInsets = UIEdgeInsetsZero
-        scrollView.contentInset = contentInset
-    }
-    
-    
-    // MARK: - Private Methods
-    
-    private func setupNotificationListenersForKeyboardEvents() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow), name:UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide), name:UIKeyboardWillHideNotification, object: nil)
     }
 
 }
