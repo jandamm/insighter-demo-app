92b1c0449a4c2e3ebe3b27ffa8372a927bec29ee
Jan Damm <jandammshaeuser@me.com>
2016-09-30 12:58:05 +0200

    Integrated transitions

--- insighter/NavigationDelegate.swift
+++ insighter/NavigationDelegate.swift
@@ -15,6 +15,22 @@ class NavigationDelegate: JDNavigationDelegate {
 	override func pushAnimator(from fromVC: UIViewController, to toVC: UIViewController) -> JDAnimator? {
 		animator.reset()
 
+		if fromVC is IntroVC && toVC is QuestionVC {
+			animator.animationType(setTo: .slideFromBottom)
+		}
+		if fromVC is IntroVC && toVC is OnboardingWelcomeVC {
+			animator.animationType(setTo: .slideFromTop)
+		}
+		if fromVC is LoginVC {
+			animator.animationType(setTo: .slideFromBottom)
+		}
+		if fromVC is EvaluationVC && toVC is LoginVC {
+			animator.animationType(setTo: .slideFromTop)
+		}
+		if fromVC is QuestionVC && !(toVC is QuestionVC) {
+			animator.animationType(setTo: .slideFromTop)
+		}
+
 		return animator
 	}
 }
